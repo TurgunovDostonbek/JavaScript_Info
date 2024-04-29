24a9fdc8abda70337222aadf66a8d920c72e72e3
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-04-30 03:45:07 +0500

    JavaScriptga kirish, Veraybls

---
+++ 1-Dars/index.js
@@ -0,0 +1,14 @@
+// let a = 0 
+// let b = 1
+// javascriptga kirish,,
+
+// let , var , const,
+// let a = 'salom'
+// const b = 'salom'
+// var c = 'salom'
+
+// console.log(a,b,c);
+
+// let a = 'salom'
+// const b = 'salom'
+// var c = 'salom'
