ab5b1bb1c5fbc713b829a3071e66a2cdaaad6ef6
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-05-01 01:05:25 +0500

    Data type...

---
+++ 2-Dars/index.js
@@ -0,0 +1,66 @@
+// let a = '   34 v3n bd nsdsnfvsd   '
+// console.log(a.split(' ').join('').trim());
+
+// let a = 'salom web'
+// let b = a.split(' ')
+// let s = []
+// for(let i =0; i<b.length; i++){
+//     let c = b[i].slice(0,1).toUpperCase()
+//     s.push(c)
+//     // console.log(c);
+//     // console.log(b[i]);
+// }
+
+
+
+// console.log(s.join('.'));
+
+// let a = "ATTG"
+// // let b = a.replaceAll('T',"U")
+// // console.log(b);
+// a.toUpperCase()
+
+// let b = a.replaceAll("T",'')
+// console.log(b);
+
+// let a = 5
+
+// let b = repeat(5)
+// console.log(b);
+
+let str = 'salom'
+console.log(typeof str);
+
+let num = 1234235
+console.log(typeof num);
+
+let bigIn = 13234532512315n
+console.log(typeof bigIn);
+
+let bool = true
+console.log(typeof bool);
+
+let undf 
+console.log(typeof undf);
+
+let nul = null
+console.log(typeof nul);
+
+let syb = Symbol('salom')
+console.log(typeof syb);
+
+// ===============
+
+let obj = {
+    name: "Dostonbek",
+    age: 26
+}
+console.log(typeof obj);
+
+let arry = [1,2,3,4,5,4,'salom']
+console.log(typeof arry);
+
+let func = () => {
+    console.log(func());
+}
+// console.log(func());
