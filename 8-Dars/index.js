11d0e39e72d8214b5ca494f0933366aa1045778f
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-05-09 02:40:10 +0500

    Basic Object

---
+++ 8-Dars/index.js
@@ -0,0 +1,30 @@
+// Basic Object, Advanced Object
+
+// JavaScript o'zgaruvchilari ko'p qiymatlarni ham o'z ichiga olishi mumkin.
+// Ob'ektlar ham o'zgaruvchilardir. Lekin ob'ektlar juda ko'p qiymatlarni o'z ichiga olishi mumkin.
+// Ob'ekt qiymatlari nom sifatida yoziladi : qiymat juftlari (nom va qiymat ikki nuqta bilan ajratilgan).
+
+// let person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+// JavaScript ob'yektlarida nomlangan qiymatlar xususiyatlar deyiladi .
+// JavaScript yordamida siz o'zingizning ob'ektlaringizni belgilashingiz va yaratishingiz mumkin.
+
+// Yangi ob'ektlarni yaratishning turli usullari mavjud:
+
+// Ob'ekt literalidan foydalanib, bitta ob'ekt yarating.
+// Kalit so'zi bilan bitta ob'ekt yarating new.
+// Ob'ekt konstruktorini aniqlang va keyin tuzilgan turdagi ob'ektlarni yarating.
+// dan foydalanib ob'ekt yarating Object.create().
+
+// const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue"
+//   };
+//   const person = new Object();
+// person.firstName = "John";
+// person.lastName = "Doe";
+// person.age = 50;
+// person.eyeColor = "blue";
