d4866660443b56c64ec59da20203289ddcb583fb
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-05-09 02:15:03 +0500

    Switch case, Ternary Operation, Nullish Operation

--- 5-Dars/index.js
+++ 5-Dars/index.js
@@ -1,2 +1,54 @@
 // Switch case, Ternary Operation, Nullish Operation 
+// let dna = "TTTG"
+// let a =  dna.replaceAll("T","U")
+// console.log(a);
+// function getFee(isMember) {
+//     return isMember ? '$2.00' : '$10.00';
+//   }
+  
+//   console.log(getFee(true));
+//   // Expected output: "$2.00"
+  
+//   console.log(getFee(false));
+//   // Expected output: "$10.00"
+  
+//   console.log(getFee(null));
+//   // Expected output: "$10.00"
 
+// function example() {
+//     return condition1 ? value1
+//       : condition2 ? value2
+//       : condition3 ? value3
+//       : value4;
+//   }
+
+
+// switch (expression) {
+//     case value1:
+//       //Здесь выполняются инструкции, если результат выражения равен value1
+//       break;
+//     case value2:
+//       //Инструкции, соответствующие value2
+//       break;
+ 
+//     case valueN:
+//       //Инструкции, соответствующие значению valueN
+//       //statementsN
+//       break;
+//     default:
+//       //Здесь находятся инструкции, которые выполняются при отсутствии соответствующего значения
+//       //statements_def
+//       break;
+//   }
+
+
+// const foo = null ?? 'default string';
+// console.log(foo);
+// // Expected output: "default string"
+
+// const baz = 0 ?? 42;
+// console.log(baz);
+// // Expected output: 0
+
+
+// Nullish coalescing (??) operatori mantiqiy operator bo‘lib, uning chap tomonidagi operand null yoki aniqlanmagan bo‘lsa, o‘zining o‘ng tomoni operandini qaytaradi, aks holda chap tomonidagi operandni qaytaradi
