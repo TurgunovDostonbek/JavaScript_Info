e885cba8d7ab74f0b536f55f4c95a267120d22c3
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-05-09 02:33:30 +0500

    Functions

---
+++ 7-Dars/index.js
@@ -0,0 +1,28 @@
+// Functions 
+// =====================
+
+// functionJavaScript funksiyasi kalit so'z, undan keyin ism va keyin qavs () bilan belgilanadi .
+
+// Funktsiya nomlarida harflar, raqamlar, pastki chiziq va dollar belgilari bo'lishi mumkin (o'zgaruvchilar bilan bir xil qoidalar).
+
+// Qavslar vergul bilan ajratilgan parametr nomlarini o'z ichiga olishi mumkin:
+// ( parametr1, parametr2, ... )
+
+// Funktsiya tomonidan bajariladigan kod jingalak qavslar ichiga joylashtiriladi: {}
+
+
+
+// Declaration function 
+
+// JavaScript funktsiyalari kalit so'z bilan aniqlanadifunction .
+// Siz funktsiya deklaratsiyasi yoki funktsiya ifodasidan foydalanishingiz mumkin .
+
+
+// function functionName(parameters) {
+//     // code to be executed
+//   }
+
+//   function myFunction(a, b) {
+//     return a * b;
+//   }
+
