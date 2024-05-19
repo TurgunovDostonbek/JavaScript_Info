305df7ee1ea654f6530188767af23225d1084d93
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-05-19 14:44:04 +0500

    new Data, Distracturing

--- 13-Dars/js.js
+++ 13-Dars/js.js
@@ -1,1 +1,75 @@
 /// new Data, Distracturing
+
+// let a = [1,2,3,4]
+
+// let b = a.reduce((sum,item,index) => sum +=  index  ,0)
+// console.log(b);
+// // for(let i = 0; i<a.length; i++){
+
+// // }
+
+// let a = [[2,3,4],[5,6,7]]
+
+// let b = a.map((item) => [item[0] * 2 ])
+// console.log(b);
+
+// const a = document.querySelector('#a') 
+// let c = new Date()
+// let soat = c.getHours()
+
+// a.textContent() = c.getHours()
+// console.log(b);
+
+// let ab  = new Date();
+
+// console.log(ab.getHours());
+// console.log(ab.getMinutes());
+// console.log(ab.getSeconds());
+// console.log(ab.getMilliseconds());
+
+// let a  = new Date()
+// console.log(a);
+
+
+// let a  = [1,2,3]
+
+// console.log(typeof a );
+
+// let a = "l"
+//  let b = a.repeat(13)
+// console.log(b);
+
+// let a = 234
+
+// let b = a.toString().split('').join("+")
+// let c = a.toString().split('').join("*")
+// console.log(eval(b));
+// console.log(eval(c));
+// console.log(c);
+let a  = 'salom sen men'
+console.log(a.split(" ").slice(0,2).join(""));
+
+
+
+// let a = [1,2,3,4,7,5]
+
+// let b = a.sort((a,b) => b - a ).slice(0,1).join("")
+// console.log(Number(b));
+//   let c = []
+
+// for(let i = 0; i<=b; i++ ){
+//   c.push(i)
+// }
+// console.log(c);
+// let n = 15
+// for(let i = 1; i<=n; i++){
+//     if(i % 3 === 0 &&  i % 5 === 0 ){
+//          console.log('FizzBuzz');
+//     }else if(i % 3 === 0){
+//       console.log( "Fizz");
+//     }else if( i % 5 == 0){
+//          console.log("Buzz");
+//     }else{
+//         console.log(i);
+//     }
+// }
