c1f1a9e9582aca1f9a5d62cd47f208e8b424542e
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-05-21 03:00:37 +0500

    RegExp... Advenced, Expert....

---
+++ 16-Dars/index.js
@@ -0,0 +1,56 @@
+// RegExp... Advenced, Expert....
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // let arr = [1,2,3,4,5,6,7,"jasfhasj", 's']
+// let arr = "44salzomjahsk27358263"
+// let n = "z"
+// let regEx = /.+/;
+
+// // let a = regEx.test(arr)
+// let a = arr.match(regEx)
+
+// // let a  = arr.map((item) => item !== regEx)
+
+// console.log(a);
+
+// Array.prototype.remove = function(n){
+//     let res = this.filter((item) => item !== n)
+//     console.log(res);
+// }
+
+// arr.remove(4)
