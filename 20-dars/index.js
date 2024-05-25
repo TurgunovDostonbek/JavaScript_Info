354eb4efd8d6af9ce46f7bfd6d5020cb33b75549
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-05-26 02:36:07 +0500

    setTimeOut  vs setInterval ..

---
+++ 20-dars/index.js
@@ -0,0 +1,36 @@
+//// setTimeOut  vs setInterval ..
+
+
+// setTimeout(callback, time, arg)  => setTimeOut bu ishga tushganda berilgan vaqt kutadi..
+
+// console.log(1);
+
+// setTimeout(() =>{
+//     console.log("after one sec");
+// },2000)
+
+// console.log(3);
+
+
+
+
+////// setInterval  => 
+
+
+
+// setInterval(() => {
+//     console.log(1);
+// }, 2000 )
+
+
+
+
+
+// let a = [1,2,'a','b']
+
+// let b = new RegExp([0-9])
+
+// a.forEach((item) => console.log(item.match(b)))
+// //  let res = a.match(b)
+// //  console.log(res)
+// console.log(m);
