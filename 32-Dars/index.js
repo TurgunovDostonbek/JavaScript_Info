b1665e2cd819b88221bd46b574d9fe7e4d1919da
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-05-26 17:33:27 +0500

    Text Content and innerHTML, setAttribute, hasAttribute and setAttribute..

---
+++ 32-Dars/index.js
@@ -0,0 +1,51 @@
+// Text Content and innerHTML, setAttribute, hasAttribute and setAttribute..
+
+// Text Content.... => text content tallab olingan elementni textini ozgartirib beradi yoki ochiradi yoki yangilaydi yoki qoshadi..
+
+// const title = document.querySelector(".title")
+// title.textContent = "Hello Javascript"
+
+// const list_item = document.querySelectorAll(".list_item")
+// list_item.forEach((item) => {
+//     item.textContent += " Darslari"
+// })
+
+
+// innerHTML.... => inner HTML esa textcontent qilgan ishlarni barchasini qiladi va qoshimcha teglar ham qabul qiladi HTML teglarini ham qabul qiladi. 
+
+//     const title = document.querySelector(".title")
+// title.innerHTML = '<i>DOM</i>'
+
+
+// let arry = [1,2,3,4,5,6]
+
+// const ol = document.querySelector("ol")
+
+// arry.forEach((item) => {
+//     ol.innerHTML += `<li>${item}</li>`
+// })
+
+
+// innerText  ham text content bilan bir hil vazifa bajaradi. deyarli bir hil.
+
+
+
+///======================================================
+
+// setAttribute and  getAttribute...
+
+// setAttribute  => bu tegni atributlarini ozgartiradi yangilaydi qo'shadi.. 
+// getAttribute  => bu teg ichidagi bor atributlarni olib beradi yoki...    
+// hasAttribute  => bu metod true yoki false qaytaradi agar berilgan attribute tegda bolsa true bolmasa false qaytaradi.
+
+// const link  = document.querySelector('a')
+// console.log(link.getAttribute('href'));
+
+// link.setAttribute("href", "https://www.yandex.ru/?hl=ru")
+
+// console.log(link);
+
+
+
+
+
