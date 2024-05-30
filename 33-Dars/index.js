4ac2d71c39786922354bb5291f4aacfb515abb9d
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-05-31 01:00:24 +0500

    add and remove toggle class..

---
+++ 33-Dars/index.js
@@ -0,0 +1,24 @@
+//  add and remove toggle class..
+
+// const content = document.querySelector("p")
+
+// console.log(content.classList.add('success'));
+// console.log(content.classList.remove('success'));
+
+// const content = document.querySelectorAll("p");
+
+// content.forEach((item) => {
+//     if(item.textContent.includes("error")){
+//         item.classList.add("error")
+//     }else if(item.textContent.includes("succes")){
+//         item.classList.add("succes")
+//     }
+// })
+
+// toggle 
+
+const content = document.querySelector("h1")
+
+// content.classList.toggle("error")
+content.classList.toggle("succes")
+
